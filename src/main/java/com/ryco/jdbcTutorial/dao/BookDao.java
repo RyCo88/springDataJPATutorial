0e9db6c55773e6934e8f9adca23c323d09575aac
Ryan Corson <ryan.corson88@gmail.com>
2025-11-03 20:08:13 -0600

    Initial commit for Spring Data JPA Tutorial

---
+++ src/main/java/com/ryco/jdbcTutorial/dao/BookDao.java
@@ -0,0 +1,18 @@
+package com.ryco.jdbcTutorial.dao;
+
+import com.ryco.jdbcTutorial.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookDao {
+    void create(Book book);
+
+    Optional<Book> findOne(String isbn);
+
+    List<Book> find();
+
+    void update(String isbn, Book book);
+
+    void delete(String isbn);
+}
---
+++ src/test/java/com/ryco/jdbcTutorial/dao/impl/BookDaoImplTests.java
@@ -0,0 +1,77 @@
+package com.ryco.jdbcTutorial.dao.impl;
+
+import com.ryco.jdbcTutorial.TestDataUtil;
+import com.ryco.jdbcTutorial.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class BookDaoImplTests {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private BookDaoImpl underTest;
+
+    @Test
+    public void testThatCreateBookGenerateCorrectSql() {
+        Book book = TestDataUtil.createTestBookA();
+
+        underTest.create(book);
+
+        verify(jdbcTemplate).update(
+                eq("INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)"),
+                eq("978-1-2345-6789-0"),
+                eq("The Shadow in the Attic"),
+                eq(1L)
+        );
+    }
+
+    @Test
+    public void testThatFindOneBookGenerateCorrectSql() {
+        underTest.findOne("987-1-2345-6789-0");
+        verify(jdbcTemplate).query(
+                eq("SELECT isbn, title, author_id FROM books WHERE isbn = ? LIMIT 1"),
+                ArgumentMatchers.<BookDaoImpl.BookRowMapper>any(),
+                eq("987-1-2345-6789-0")
+        );
+    }
+
+    @Test
+    public void testThatFindGeneratesCorrectSql() {
+        underTest.find();
+        verify(jdbcTemplate).query(
+                eq("SELECT isbn, title, author_id from books"),
+                ArgumentMatchers.<BookDaoImpl.BookRowMapper>any()
+        );
+    }
+
+    @Test
+    public void testThatUpdateGeneratesTheCorrectSql() {
+        Book book = TestDataUtil.createTestBookA();
+        underTest.update("978-1-2345-6789-0", book);
+        verify(jdbcTemplate).update(
+                "UPDATE books SET isbn = ?, title = ?, author_id = ? WHERE isbn =?",
+                "978-1-2345-6789-0", "The Shadow in the Attic", 1L, "978-1-2345-6789-0"
+        );
+    }
+
+    @Test
+    public void testThatBookDeleteGeneratesCorrectSql() {
+        underTest.delete("987-1-2345-6789-0");
+        verify(jdbcTemplate).update(
+                "DELETE FROM books WHERE isbn = ?",
+                "987-1-2345-6789-0"
+        );
+    }
+
+}
